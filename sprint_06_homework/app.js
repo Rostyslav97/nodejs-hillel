ff8cbd26e8ab4b6c849630c86347de527685be04
Rostyslav Malaniuk <malanyukrostik@gmail.com>
2026-01-28 13:23:05 +0200

    add hw-5

---
+++ sprint_06_homework/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import homeRouter from './routers/home.js';
+// import categoryRouter from './routers/category.js';
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+const PORT = process.env.PORT || 3000;
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, 'public')));
+
+
+app.use('/', homeRouter);
+// app.use('/category', categoryRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server start на http://localhost:${PORT}`);
+});
